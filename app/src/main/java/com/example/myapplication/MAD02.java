6c4d1bf89336e4c17158dfd2fd634e2a7c77649c
acer <binugunawardana0@gmail.com>
2020-03-19 16:47:33 +0530

    Mad project 01

---
+++ app/src/main/java/com/example/myapplication/MAD02.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAD02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_a_d02);
+    }
+}
